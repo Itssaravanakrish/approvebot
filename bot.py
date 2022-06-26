f9955b58048068d731efdc44cc2c411b67708c2d
Itssaravanakrish <77931259+Itssaravanakrish@users.noreply.github.com>
2022-06-26 22:52:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, User, ChatJoinRequest
 
-pr0fess0r_99=Client(
+tamilbots=Client(
     "Auto Approved Bot",
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
@@ -13,22 +13,22 @@ CHAT_ID=int(os.environ.get("CHAT_ID", None))
 TEXT=os.environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour Auto Approved")
 APPROVED = os.environ.get("APPROVED_WELCOME", "on").lower()
 
-@pr0fess0r_99.on_message(filters.private & filters.command(["start"]))
-async def start(client: pr0fess0r_99, message: Message):
+@tamilbots.on_message(filters.private & filters.command(["start"]))
+async def start(client: tamilbots, message: Message):
     approvedbot = await client.get_me() 
     button=[[
-      InlineKeyboardButton("Support Group 👥", url="https://t.me/No1_Bots_SupportGroup"),
-      InlineKeyboardButton("Updates Channel 📢", url="https://t.me/No1_Bots")
+      InlineKeyboardButton("Support Group 👥", url="https://t.me/TamilSupport"),
+      InlineKeyboardButton("Updates Channel 📢", url="https://t.me/TamilBots")
       ],[
-      InlineKeyboardButton("Bot List 🤖", url=f"https://t.me/No1_Bots/11")
+      InlineKeyboardButton("Bot List 🤖", url=f"https://t.me/TamilBots/84")
       ]]
     await message.reply_text(text="""**__Hεүү, Iam Auto Approved Join Request Bot
 
 • I can Auto approve new join requests In Channels And Groups
 • Make Me Admin In Ur Channel Or Group With Invite Users Permission, Then See The magic ✨.**__""", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
 
-@pr0fess0r_99.on_chat_join_request(filters.chat(CHAT_ID))
-async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
+@tamilbots.on_chat_join_request(filters.chat(CHAT_ID))
+async def autoapprove(client: tamilbots, message: ChatJoinRequest):
     chat=message.chat # Chat
     user=message.from_user # User
     print(f"{user.first_name} Joined 🤝") # Logs
@@ -38,4 +38,4 @@ async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
         print("Welcome....")
 
 print("Auto Approved Bot")
-pr0fess0r_99.run()
+tamilbots.run()
